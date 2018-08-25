11808c5a7c36e6a317a7d3dc1ebc7d785c696506
Michael Saylor <saylormic@gmail.com>
2018-08-25 16:24:12 -0400

    followed video instructions

--- app/app.modules.ts
+++ app/app.modules.ts
@@ -1,16 +1,17 @@
+import { ArtistDetailsComponent } from './component.artist-details';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './component.app';
-import { ArtistItemComponent } from './component.artist-item';
+import { ArtistItemComponent } from './component.artist-item'
 
 @NgModule({
   imports: [
     BrowserModule, FormsModule
   ],
   declarations: [
-    AppComponent, ArtistItemComponent
+    AppComponent, ArtistItemComponent, ArtistDetailsComponent
   ],
   bootstrap: [
     AppComponent
