a9c03fa04ea0782baeb92d1a14f6e26a2f00cefe
Michael Saylor <saylormic@gmail.com>
2018-08-25 11:51:55 -0400

    followed video instructions

--- app/component.app.ts
+++ app/component.app.ts
@@ -5,4 +5,25 @@ import { Component } from '@angular/core';
   templateUrl: './partials/app.html' 
 })
 
-export class AppComponent {}
+export class AppComponent {
+  name: string;
+  artists: any;
+
+  constructor() {
+    this.name = 'Xhou Ta';
+    this.artists = [ 
+      {
+        name: 'Barot Bellingham',
+        school: 'Penn State'
+      },
+      {
+        name: 'Jonathan Ferrar',
+        school: 'University of Illinois'
+      }, 
+      {
+        name: 'Hillary Post',
+        school: 'University of Florida'
+      }
+    ]
+  }
+}
